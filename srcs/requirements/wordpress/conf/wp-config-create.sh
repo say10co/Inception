8150064b238d2fdfd0e89d05f492325b8ac28f1c
say10co <driouchayoubdri@gmail.com>
2023-03-08 13:39:45 -0500

    adding wordpres

---
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -0,0 +1,4 @@
+
+sed -i 's/database_name_here/${DB_NAME}/g' /var/www/wordpress/wp-config-sample.php  
+sed -i 's/username_here/${DB_USER_NAME}/g' /var/www/wordpress/wp-config-sample.php 
+sed -i 's/password_here/${DB_USER_PASS}/g' /var/www/wordpress/wp-config-sample.php 
