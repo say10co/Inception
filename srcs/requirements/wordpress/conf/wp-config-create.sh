e6f44c903fc9a1fb45569e91832941291c479549
say10co <driouchayoubdri@gmail.com>
2023-03-19 01:01:10 +0000

    wordpress config file auto fill reamaing, kudo for the others

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,4 +1,9 @@
 
-sed -i 's/database_name_here/${DB_NAME}/g' /var/www/wordpress/wp-config-sample.php  
-sed -i 's/username_here/${DB_USER_NAME}/g' /var/www/wordpress/wp-config-sample.php 
-sed -i 's/password_here/${DB_USER_PASS}/g' /var/www/wordpress/wp-config-sample.php 
+#fix this, the file is config file is filled manually at the moment 
+#
+#
+#
+#sed -i "s/database_name_here/test/g" /var/www/wordpress/wp-config-sample.php  
+#sed -i "s/username_here/$DB_USER_NAME/g" /var/www/wordpress/wp-config-sample.php 
+#sed -i "s/password_here/$DB_USER_PASS/g" /var/www/wordpress/wp-config-sample.php 
+cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php  
