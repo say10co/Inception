2a57e998b2c62d60813ca87641ca9d60f6fd76a3
say10co <driouchayoubdri@gmail.com>
2023-03-21 04:09:56 -0400

    verifing

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -6,4 +6,4 @@
 #sed -i "s/database_name_here/test/g" /var/www/wordpress/wp-config-sample.php  
 #sed -i "s/username_here/$DB_USER_NAME/g" /var/www/wordpress/wp-config-sample.php 
 #sed -i "s/password_here/$DB_USER_PASS/g" /var/www/wordpress/wp-config-sample.php 
-cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php  
+#cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php  
